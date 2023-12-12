32449856d791ab271362bc9b4a38b2e7568b5a06
jsepucas <jsepucas@myuax.com>
2023-12-12 13:19:51 +0100

    5.4

--- 5_Estructuras&Funciones.cpp
+++ 5_Estructuras&Funciones.cpp
@@ -18,3 +18,13 @@ void modificarEstudiante(Estudiante* est) {
 void imprimirEstudiante2(const Estudiante* est) {
     std::cout << "Nombre: " << est->nombre << ", Edad: " << est->edad << ", Promedio: " << est->promedio << std::endl;
 }
+
+int main() {
+    Estudiante estudiante1;
+    strcpy(estudiante1.nombre, "Jose");
+    estudiante1.edad = 18;
+    estudiante1.promedio = 9;
+
+    imprimirEstudiante(estudiante1);
+    modificarEstudiante(&estudiante1);
+    imprimirEstudiante2(&estudiante1);
