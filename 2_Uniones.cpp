b28ee26d701bb922dbecc2cc85352d7ce0cbf79b
jsepucas <jsepucas@myuax.com>
2023-12-12 13:01:04 +0100

    3.1

--- 2_Uniones.cpp
+++ 2_Uniones.cpp
@@ -7,3 +7,17 @@ union Datos{
     char Caracter[30];
 };
 
+int main(){
+    union Datos uni;
+
+    uni.Entero = 99;
+    printf("Entero: %d\n", uni.Entero);
+
+    uni.Flotante = 3.1416;
+    printf("Flotante: %.2f\n", uni.Flotante);
+
+    strcpy(uni.Caracter, "Hasta luego");
+    printf("Caracter: %s\n", uni.Caracter);
+
+    return 0;
+}
