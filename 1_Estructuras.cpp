40f2846b2dc25474643a2b06a2e1ec82730d98cd
jsepucas <jsepucas@myuax.com>
2023-12-12 12:52:44 +0100

    11

--- 1_Estructuras.cpp
+++ 1_Estructuras.cpp
@@ -1,5 +1,18 @@
-#include <iostream>
-#include <string>
-#include <vector>
+struct Estudiante{
+    char nombre[20];
+    int edad;
+    float promedio;
+};
 
-using namespace std;
+int main(){
+    struct Estudiante estudiante1;
+    strcpy(estudiante1.nombre, "Mario");
+    estudiante1.edad = 18;
+    estudiante1.promedio = 8.47;
+
+    struct Estudiante* estudiantePtr;
+    estudiantePtr = (struct Estudiante*)malloc(sizeof(struct Estudiante));
+    if (estudiantePtr == nullptr) {
+        fprintf(stderr, "Error al asignar memoria.\n");
+        return 1;
+    }
