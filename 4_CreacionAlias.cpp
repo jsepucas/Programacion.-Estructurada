509d5fea0a6ff714e6a00e86f573acb4b18c07b9
jsepucas <jsepucas@myuax.com>
2023-12-12 13:09:59 +0100

    4.2

--- 4_CreacionAlias.cpp
+++ 4_CreacionAlias.cpp
@@ -6,3 +6,12 @@ struct Estudiante {
     int edad;
     float promedio;
 };
+
+typedef struct Estudiante EstudianteAlias;
+
+EstudianteAlias obtenerCopiaYMostrarAlias(const Estudiante& estudiante) {
+
+    std::cout << "Estudiante Inicial:" << std::endl;
+    std::cout << "Nombre: " << estudiante.nombre << std::endl;
+    std::cout << "Edad: " << estudiante.edad << std::endl;
+    std::cout << "Promedio: " << estudiante.promedio << std::endl;
