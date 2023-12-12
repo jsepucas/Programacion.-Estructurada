0b915069f4d8a043b08467fcc36d6f96e8d93136
jsepucas <jsepucas@myuax.com>
2023-12-12 11:59:31 +0100

    4

--- 1_Estructuras.cpp
+++ 1_Estructuras.cpp
@@ -11,3 +11,8 @@ struct Estudiante {
     float promedio;
 };
 
+void agregarEstudiante(vector<Estudiante*>& lista);
+void mostrarEstudiantes(const vector<Estudiante*>& lista);
+void eliminarEstudiante(vector<Estudiante*>& lista);
+Estudiante* crearEstudiante(const string& nombre, int edad, float promedio);
+
