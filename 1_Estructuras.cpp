a7a7eb487298cbd5d6636204338ced82d302f226
jsepucas <jsepucas@myuax.com>
2023-12-12 12:13:13 +0100

    10

--- 1_Estructuras.cpp
+++ 1_Estructuras.cpp
@@ -1,54 +1,5 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cstdlib>
 
 using namespace std;
-
-struct Estudiante {
-    string nombre;
-    int edad;
-    float promedio;
-};
-
-void agregarEstudiante(vector<Estudiante*>& lista);
-void mostrarEstudiantes(const vector<Estudiante*>& lista);
-void eliminarEstudiante(vector<Estudiante*>& lista);
-Estudiante* crearEstudiante(const string& nombre, int edad, float promedio);
-
-int main() {
-    vector<Estudiante*> listaEstudiantes;
-    int opcion;
-
-    do {
-
-        cout << "1. Agregar Estudiante\n2. Enseñar Estudiantes\n3. Eliminar Estudiante\n4. Salir\n";
-        cout << "Introduzca una opcion: ";
-        cin >> opcion;
-
-        switch (opcion) {
-
-            case 1 :
-                agregarEstudiante(listaEstudiantes);
-                break;
-            case 2:
-                mostrarEstudiantes(listaEstudiantes);
-                break;
-            case 3:
-                eliminarEstudiante(listaEstudiantes);
-                break;
-            case 4:
-                break;
-            default:
-                cout << "Opcion invalida\n";
-
-        }
-    } while (opcion != 4);
-
-    for (Estudiante* est: listaEstudiantes) {
-        free(est) ;
-    }
-
-    return 0;
-
-}
