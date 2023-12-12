0643ff61abe4258dce76d68fc2de952f785b6212
jsepucas <jsepucas@myuax.com>
2023-12-12 13:15:05 +0100

    5.1

--- 5_Estructuras&Funciones.cpp
+++ 5_Estructuras&Funciones.cpp
@@ -6,3 +6,15 @@ struct Estudiante {
     int edad;
     float promedio;
 };
+
+void imprimirEstudiante(Estudiante est) {
+    std::cout << "Nombre: " << est.nombre << ", Edad: " << est.edad << ", Promedio: " << est.promedio << std::endl;
+}
+
+void modificarEstudiante(Estudiante* est) {
+    est->edad = 30;
+}
+
+void imprimirEstudiante2(const Estudiante* est) {
+    std::cout << "Nombre: " << est->nombre << ", Edad: " << est->edad << ", Promedio: " << est->promedio << std::endl;
+}
