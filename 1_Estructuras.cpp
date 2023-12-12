9982bf31a3ced7209b8c3cd5b68c50debc9d68eb
jsepucas <jsepucas@myuax.com>
2023-12-12 12:01:11 +0100

    5

--- 1_Estructuras.cpp
+++ 1_Estructuras.cpp
@@ -16,3 +16,9 @@ void mostrarEstudiantes(const vector<Estudiante*>& lista);
 void eliminarEstudiante(vector<Estudiante*>& lista);
 Estudiante* crearEstudiante(const string& nombre, int edad, float promedio);
 
+int main() {
+    vector<Estudiante*> listaEstudiantes;
+    int opcion;
+
+
+
