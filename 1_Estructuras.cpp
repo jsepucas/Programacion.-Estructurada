5f8cbbee794a06c4fe20f125f481efa9396e1451
jsepucas <jsepucas@myuax.com>
2023-12-12 12:54:47 +0100

    12

--- 1_Estructuras.cpp
+++ 1_Estructuras.cpp
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 struct Estudiante{
     char nombre[20];
     int edad;
@@ -16,3 +21,22 @@ int main(){
         fprintf(stderr, "Error al asignar memoria.\n");
         return 1;
     }
+
+    strcpy(estudiantePtr->nombre, "Sergio");
+    estudiantePtr->edad = 19;
+    estudiantePtr->promedio = 5.38;
+
+    printf("Estudiante 1:\n");
+    printf("Nombre: %s\n", estudiante1.nombre);
+    printf("Edad: %d\n", estudiante1.edad);
+    printf("Promedio: %.2f\n\n", estudiante1.promedio);
+
+    printf("Estudiante 2:\n");
+    printf("Nombre: %s\n", estudiantePtr->nombre);
+    printf("Edad: %d\n", estudiantePtr->edad);
+    printf("Promedio: %.2f\n\n", estudiantePtr->promedio);
+
+    free(estudiantePtr);
+
+
+}
