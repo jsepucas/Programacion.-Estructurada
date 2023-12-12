23251efdb275f6938b06f73b7c779bf0a32c836b
jsepucas <jsepucas@myuax.com>
2023-12-12 13:04:09 +0100

    3.4

--- 3_CopiaEstructuras.cpp
+++ 3_CopiaEstructuras.cpp
@@ -18,4 +18,17 @@ Estudiante copiarEstudiante(const Estudiante& original) {
     return copia;
 }
 
+int main() {
+    Estudiante estudiante1;
+    strcpy(estudiante1.nombre, "Adriana");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 8.5;
+
+    Estudiante copiaEstudiante1 = copiarEstudiante(estudiante1);
+
+    std::cout << "Estudiante 1:" << std::endl;
+    std::cout << "Nombre: " << estudiante1.nombre << std::endl;
+    std::cout << "Edad: " << estudiante1.edad << std::endl;
+    std::cout << "Promedio: " << estudiante1.promedio << std::endl << std::endl;
+
 
